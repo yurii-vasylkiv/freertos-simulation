8031fe0f9c6352e1efb2c9b6acc0e05f7d32ea72
Yurii Vasylkiv <yurii.vasylkiv@gmail.com>
2020-10-26 11:03:17 -0500

    removed files from generated folder

--- generated/CMDConfigFile.cmd
+++
@@ -1,4 +0,0 @@
-set MINGW_ENV_PATH= "C:\root\usr\bin"
-set CLION_CMAKE_PATH= "C:\root\usr\bin"
-set ARM_TOOLS_DIR= "C:\root\usr\bin"
-set EABI_PREFIX= "arm-atollic-eabi"
--- generated/CMakeConfigFile.cmake
+++
@@ -1,4 +0,0 @@
-SET(MINGW_ENV_PATH "C:/root/usr/bin")
-SET(CLION_CMAKE_PATH "C:/root/usr/bin")
-SET(ARM_TOOLS_DIR "C:/root/usr/bin")
-SET(EABI_PREFIX "arm-atollic-eabi")
